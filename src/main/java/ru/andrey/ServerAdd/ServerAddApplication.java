8aed1ac5a1bdafa5bd5e3bb2bf6274d403def18c
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-03-02 13:24:56 +0300

    Initial commit

---
+++ src/main/java/ru/andrey/ServerAdd/ServerAddApplication.java
@@ -0,0 +1,13 @@
+package ru.andrey.ServerAdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andrey/ServerAdd/ServerAddApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andrey.ServerAdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
